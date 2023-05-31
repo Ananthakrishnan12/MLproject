18206712e007e32f7e1e378e69c76180810c6af8
Ananthakrishnan12 <ananthmle@gmail.com>
2023-05-31 22:29:18 +0530

    evaluation

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -2,7 +2,7 @@ import os
 import sys
 from dataclasses import dataclass
 
-from catboost import catboostregressor
+from catboost import CatBoostRegressor
 from sklearn.ensemble import (
     AdaBoostRegressor,GradientBoostingRegressor,RandomForestRegressor
 ) 
@@ -15,7 +15,7 @@ from sklearn.metrics import r2_score
 from src.exception import CustomException
 from src.logger import logging
 
-from src.utils import save_object
+from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
@@ -42,8 +42,34 @@ class ModelTrainer:
                 "GradientBoostingRegressor":GradientBoostingRegressor(),
                 "Adaboostregressor":AdaBoostRegressor(),
                 "XgboostRegressor":XGBRegressor(),
-                "catboostregressor":catboostregressor(),
+                "catboostregressor":CatBoostRegressor(),
                 "KnearestRegressor":KNeighborsRegressor(),
             }
-        except:
-            pass
+            
+            model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+            
+            ## To get the best model score for the dict:
+            best_model_score=max(sorted(model_report.values()))
+            
+            ## To get the Best model name for the dict:
+            best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
+            best_model=models[best_model_name]
+            
+            if best_model_score<0.6:
+                raise CustomException("No best model found")
+            
+            logging.info(f"Best model found on Training and testing Dataset")
+            
+            
+            save_object(
+                file_path=self.model_trainer_config.trained_model_file_path,
+                obj=best_model
+            )
+            predicted=best_model.predict(X_test)
+            
+            r2_square=r2_score(y_test,predicted)
+            
+            return r2_square
+            
+        except Exception as e:
+            raise CustomException(e,sys)
